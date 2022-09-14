38905028b1ace74cfcda2f4456f91f74cda9dd86
Rokas <rookas.rudzenskas@gmail.com>
2022-09-14 16:13:45 +0300

    Rokas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,54 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
+import Header from "../components/Header";
 
-
+const style = {
+    wrapper: `bg-[#18191a] min-h-screen duration-[0.5s]`,
+    homeWrapper: `flex`,
+    center: `flex-1`,
+    main: `flex-1 flex justify-center`,
+    signupContainer: `flex items-center justify-center w-screen h-[70vh]`,
+}
 
 const Home: NextPage = () => {
-  return (
-    <div className="">
-      <Head>
-        <title>Facebook Web3</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const [registered, setRegistered] = useState(false)
+    const [name, setName] = useState('')
+    const [url, setUrl] = useState('')
+    const [users, setUsers] = useState([])
 
-      Hello, we are running!
+  return (
+   <div className={style.wrapper}>
+       <Head>
+           <title>Facebook Web3</title>
+           <link rel="icon" href="/favicon.ico" />
+       </Head>
+        <Header name={name} url={url} />
+
+        {registered ? (
+            <div className={style.homeWrapper}>
+                {/*<Sidebar name={name} url={url} />*/}
+                <div className={style.main}>
+                    {/*<Feed connected={wallet.connected} name={name} url={url} />*/}
+                </div>
+                {/*<RightSidebar*/}
+                {/*    getUsers={requestUsersData}*/}
+                {/*    users={users}*/}
+                {/*    setUsers={setUsers}*/}
+                {/*/>*/}
+            </div>
+        ) : (
+            <div className={style.signupContainer}>
+                {/*<SignUp*/}
+                {/*    setRegistered={setRegistered}*/}
+                {/*    name={name}*/}
+                {/*    setName={setName}*/}
+                {/*    url={url}*/}
+                {/*    setUrl={setUrl}*/}
+                {/*/>*/}
+            </div>
+        )}
     </div>
   )
 }
