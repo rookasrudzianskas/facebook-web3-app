055e71292554b187e7cf20a396d694bd1f61e17e
Rokas <rookas.rudzenskas@gmail.com>
2022-09-15 10:05:46 +0300

    Rokas

--- components/CommentSection.js
+++ components/CommentSection.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import Comment from './Comment'
+import CreateComment from './CreateComment'
 
-const CommentsSection = () => {
+const CommentSection = ({
+                            comments,
+                            createCommentForPost,
+                            name,
+                            url,
+                        }) => {
+    const style = {
+        wrapper: `w-full rounded-b-lg p-[5px] flex justify-center-center flex-col border-t border-gray-300 border-[#3a3b3e] pt-4`,
+    }
+
     return (
-        <div>
-            
+        <div className={style.wrapper}>
+            {comments.map((comment, index) => (
+                <Comment comment={comment} key={index} />
+            ))}
+            <CreateComment
+                createCommentForPost={createCommentForPost}
+                name={name}
+                url={url}
+            />
         </div>
-    );
-};
+    )
+}
 
-export default CommentsSection;
+export default CommentSection
